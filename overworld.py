278cf8172432824209e143448c1e8ba810dba974
StrydrS <38505104+StrydrS@users.noreply.github.com>
2024-01-22 05:55:46 -0700

    2

---
+++ overworld.py
@@ -0,0 +1,66 @@
+import pygame
+from menu_data import levels
+
+class Node(pygame.sprite.Sprite):
+    def __init__(self, pos, status):
+        super().__init__()
+        self.image = pygame.Surface((100,80))
+        if status == 'available':
+            self.image.fill('red')
+        elif status == 'locked':
+            self.image.fill('grey')
+        self.rect = self.image.get_rect(center = pos)
+
+class Icon(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill('blue')
+        self.rect = self.image.get_rect(center = pos)
+       
+class Overworld:
+    def __init__(self, start_level, max_level, surface): 
+        
+        #setup
+        self.display_surface = surface
+        self.max_level = max_level
+        self.current_level = start_level
+        
+        #sprites 
+        self.setup_nodes()
+        self.setup_icon()
+        
+    def setup_nodes(self):
+        self.nodes = pygame.sprite.Group()
+        
+        for index, node_data in enumerate(levels.values()):
+            if index <= self.max_level:
+                node_sprite = Node(tuple(node_data['node_pos']), 'available')
+            elif index > self.max_level:
+                node_sprite = Node(tuple(node_data['node_pos']),'locked')
+            self.nodes.add(node_sprite)
+            
+    
+    def setup_icon(self):
+        self.icon = pygame.sprite.GroupSingle()
+        icon_sprite = Icon(self.nodes.sprites()[self.current_level].rect.center)
+        self.icon.add(icon_sprite)
+    
+    def draw_paths(self):
+        points = [tuple(node['node_pos']) for index, node in enumerate(levels.values()) if index <= self.max_level]
+        pygame.draw.lines(self.display_surface, 'red', False, points, 6)
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.current_level += 1
+        elif keys[pygame.K_d]:
+            self.current_level -= 1
+            
+                
+    def run(self):
+        self.input()
+        self.draw_paths()
+        self.nodes.draw(self.display_surface)
+        self.icon.draw(self.display_surface)
+    
